084dc1535a9f750399fedb9748c99f42c72416de
Tuan Nguyen <Nguyenvantuan.itus@gmail.com>
2016-10-09 21:42:30 +0700

    Catch exception use Crashlytics

--- app/src/main/java/com/blueeagle/realm/MainActivity.java
+++ app/src/main/java/com/blueeagle/realm/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -338,7 +339,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             try {
                 Thread.sleep(3000);
             } catch (Exception e) {
-
+                Log.e("MainActivity", e.getMessage().toString());
             }
 
             return "OK";
